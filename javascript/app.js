9f638413fbfa3d2fe4e7b594a822833e13464320
Andrew P <andrewpetersondev@gmail.com>
2019-06-11 22:14:12 -0500

    added validation to some inputs

--- javascript/app.js
+++ javascript/app.js
@@ -15,6 +15,16 @@ firebase.initializeApp(firebaseConfig);
 // create a variable to manipulate firebase
 var database = firebase.database();
 
+$("#start-input").mask("##:##");
+$("#frequency-input").mask("###0");
+$("#train-name-input").mask("Z", {
+    translation: {
+        'Z': {
+            pattern: /[a-z A-Z]/, recursive: true
+        }
+    }
+}
+);
 // // Initial Values
 // var name;
 // var destination;
@@ -77,22 +87,48 @@ database.ref().on("child_added", function (childSnapshot) {
     // store everything in variables
 
     var tName = childSnapshot.val().name;
-    // console.log("NAME = " + tName);
+    // console.log("tName = " + tName);
 
     var tDestination = childSnapshot.val().destination;
-    // console.log("DESTINATION = " + tDestination);
-
-    var tStart = childSnapshot.val().start;
-    // console.log("START = " + tStart);
+    // console.log("tDestination = " + tDestination);
 
     var tFrequency = childSnapshot.val().frequency;
-    // console.log("FREQUENCY = " + tFrequency);
+    // console.log("tFrequency = " + tFrequency);
+
+    var tStart = childSnapshot.val().start;
+    console.log("tStart = " + tStart);
+
+    var timeArray = tStart.split(":");
+
+    // trainTime is converted to hours and minutes
+    var trainTime = moment().hours(timeArray[0]).minutes(timeArray[1]);
+    console.log("trainTime = " + trainTime);
+
+    // Returns the maximum (most distant future) of the given moment instances.
+    var maxMoment = moment.max(moment(), trainTime);
+    console.log("maxMoment = " + maxMoment);
+
+    var tMinutes;
+    var tArrival;
+
+    // consider the first train time has not happened yet 
+    // format tArrival to
+    // calculate tMinutes // tMinutes is the difference between current time and trainTime
+    if (maxMoment === trainTime) {
+        tArrival = trainTime.format("HH:mm A");
+        tMinutes = trainTime.diff(moment(), "minutes");
+    } else {
+
+    }
+
+
+    // ======================================================================================================================================================
 
     // first train time is pushed back 1 year to make sure it comes before the current time
     var firstTrainTimeConverted = moment(tStart, "HH:mm").subtract(1, "years");
+    console.log(firstTrainTimeConverted);
 
-    // current time
-    var currentTime = moment();
+    // var currentTime = moment();
     // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     // Difference between the times
@@ -112,6 +148,8 @@ database.ref().on("child_added", function (childSnapshot) {
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
     var nextArrival = moment(nextTrain).format("HH:mm A");
+
+    // ======================================================================================================================================================
 
     // display to html with dynamic rows
     var newRow = $("<tr>").append(
