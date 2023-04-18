b8cf29381d8f81eaa4126c5b0f3f658a6a0ce43b
Andrew Peterson <andrewpetersondev@gmail.com>
2023-04-18 17:24:53 -0500

    secured api key

--- README.md
+++ README.md
@@ -2,38 +2,8 @@
 
 ## Take a look
 
-__[Basic Train Scheduler](https://andrewpetersondev.github.io/basic-train-scheduler/)__
+**[Basic Train Scheduler](https://andrewpetersondev.github.io/basic-train-scheduler/)**
 
 ## Overview
 
 This is a train schedule application that incorporates Firebase to host arrival and departure data. The app will retrieve and manipulate this information with Moment.js. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
-
-## Instructions
-
-### Basic Specs
-
-1. [] When adding trains, administrators are able to submit the following:
-    * [] Train Name
-    * [] Destination
-    * [] First Train Time -- in military time
-    * [] Frequency -- in minutes
-
-2. [] This app calculates when the next train will arrive relative to the current time
-
-3. [] Users from many different machines are able to view same train times.
-
-## Bonus
-
-1. [] Consider updating your "minutes to arrival" and "next train time" text once every minute. This is significantly more challenging; only attempt this if you've completed the actual activity and committed it somewhere on GitHub for safekeeping (and maybe create a second GitHub repo).
-
-2. [] Try adding update and remove buttons for each train. Let the user edit the row's elements-- allow them to change a train's Name, Destination and Arrival Time (and then, by relation, minutes to arrival).
-
-3. [] As a final challenge, make it so that only users who log into the site with their Google or GitHub accounts can use your site. You'll need to read up on Firebase authentication for this bonus exercise.
-
-## Status
-
-In Progress.
-
-## Sources
-
-This project may have code from class activities, office hours notes, or from seeking help from TAs and teachers.
--- javascript/app.js
+++ javascript/app.js
@@ -1,30 +1,30 @@
 // configure firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyCEwL4pz_xTOsOzHa8WDuFzw0KpWCW-2w8",
-    authDomain: "train-tracker-b625f.firebaseapp.com",
-    databaseURL: "https://train-tracker-b625f.firebaseio.com",
-    projectId: "train-tracker-b625f",
-    storageBucket: "train-tracker-b625f.appspot.com",
-    messagingSenderId: "176834673091",
-    appId: "1:176834673091:web:9cdb48a74d3151d3"
-};
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+}
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 // create a variable to manipulate firebase
-var database = firebase.database();
+var database = firebase.database()
 
-$("#start-input").mask("##:##");
-$("#frequency-input").mask("###0");
+$("#start-input").mask("##:##")
+$("#frequency-input").mask("###0")
 $("#train-name-input").mask("Z", {
-    translation: {
-        'Z': {
-            pattern: /[a-z A-Z]/, recursive: true
-        }
-    }
-}
-);
+  translation: {
+    Z: {
+      pattern: /[a-z A-Z]/,
+      recursive: true,
+    },
+  },
+})
 // // Initial Values
 // var name;
 // var destination;
@@ -32,98 +32,96 @@ $("#train-name-input").mask("Z", {
 // var frequency;
 
 function clearForm() {
-    $("#train-name-input").val("");
-    $("#destination-input").val("");
-    $("#start-input").val("");
-    $("#frequency-input").val("");
-};
+  $("#train-name-input").val("")
+  $("#destination-input").val("")
+  $("#start-input").val("")
+  $("#frequency-input").val("")
+}
 
 // button to add train
 $("#add-train-btn").on("click", function (event) {
+  // prevent default because it is a form
+  event.preventDefault()
 
-    // prevent default because it is a form
-    event.preventDefault();
+  // grab user input
+  var name = $("#train-name-input").val().trim()
+  var destination = $("#destination-input").val().trim()
+  var start = $("#start-input").val().trim()
+  var frequency = $("#frequency-input").val().trim()
 
-    // grab user input 
-    var name = $("#train-name-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var start = $("#start-input").val().trim();
-    var frequency = $("#frequency-input").val().trim();
+  // create local temporary object for holding data
+  var newTrain = {
+    name: name,
+    destination: destination,
+    start: start,
+    frequency: frequency,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP,
+  }
 
-    // create local temporary object for holding data
-    var newTrain = {
-        name: name,
-        destination: destination,
-        start: start,
-        frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    };
+  // log everything
+  console.log(newTrain)
 
-    // log everything
-    console.log(newTrain);
+  // upload train data to firebase
+  database.ref().push(newTrain)
 
-    // upload train data to firebase
-    database.ref().push(newTrain);
+  // log everything
+  // console.log(newTrain);
 
-    // log everything
-    // console.log(newTrain);
-
-    clearForm();
-
-});
+  clearForm()
+})
 
 // firebase event for adding trains and a dynamic row in the html when user enters a new train
 // firebase docs refer to data returned as childSnapshot
 // in this case the name doesn't matter but use childSnapshot anyways
 // all data returned from firebase is in the form of a string
-database.ref().on("child_added", function (childSnapshot) {
-
+database.ref().on(
+  "child_added",
+  function (childSnapshot) {
     // data returned from firebase
     // console.log(childSnapshot);
 
     // firebase documentation says all data stored is accessed in val() method
-    console.log(childSnapshot.val());
+    console.log(childSnapshot.val())
 
     // store everything in variables
 
-    var tName = childSnapshot.val().name;
+    var tName = childSnapshot.val().name
     // console.log("tName = " + tName);
 
-    var tDestination = childSnapshot.val().destination;
+    var tDestination = childSnapshot.val().destination
     // console.log("tDestination = " + tDestination);
 
-    var tFrequency = childSnapshot.val().frequency;
+    var tFrequency = childSnapshot.val().frequency
     // console.log("tFrequency = " + tFrequency);
 
-    var tStart = childSnapshot.val().start;
-    console.log("tStart = " + tStart);
+    var tStart = childSnapshot.val().start
+    console.log("tStart = " + tStart)
 
-    var timeArray = tStart.split(":");
+    var timeArray = tStart.split(":")
 
     // trainTime is converted to hours and minutes
-    var trainTime = moment().hours(timeArray[0]).minutes(timeArray[1]);
-    console.log("trainTime = " + trainTime);
+    var trainTime = moment().hours(timeArray[0]).minutes(timeArray[1])
+    console.log("trainTime = " + trainTime)
 
     // Returns the maximum (most distant future) of the given moment instances.
-    var maxMoment = moment.max(moment(), trainTime);
-    console.log("maxMoment = " + maxMoment);
+    var maxMoment = moment.max(moment(), trainTime)
+    console.log("maxMoment = " + maxMoment)
 
-    var tMinutes;
-    var tArrival;
+    var tMinutes
+    var tArrival
 
-    // consider the first train time has not happened yet 
+    // consider the first train time has not happened yet
     // format tArrival to
     // calculate tMinutes // tMinutes is the difference between current time and trainTime
     if (maxMoment === trainTime) {
-        tArrival = trainTime.format("HH:mm A");
-        tMinutes = trainTime.diff(moment(), "minutes");
+      tArrival = trainTime.format("HH:mm A")
+      tMinutes = trainTime.diff(moment(), "minutes")
     } else {
-        var diffTime = moment().diff(trainTime, "minutes");
-        var tRemainder = diffTime % tFrequency;
-        tMinutes = tFrequency - tRemainder;
-        tArrival = moment().add(tMinutes, "m").format("hh:mm A");
+      var diffTime = moment().diff(trainTime, "minutes")
+      var tRemainder = diffTime % tFrequency
+      tMinutes = tFrequency - tRemainder
+      tArrival = moment().add(tMinutes, "m").format("hh:mm A")
     }
-
 
     // ======================================================================================================================================================
 
@@ -156,17 +154,18 @@ database.ref().on("child_added", function (childSnapshot) {
 
     // display to html with dynamic rows
     var newRow = $("<tr>").append(
-        $("<td>").text(tName),
-        $("<td>").text(tDestination),
-        $("<td>").text(tFrequency),
-        $("<td>").text(tArrival),
-        $("<td>").text(tMinutes)
-    );
+      $("<td>").text(tName),
+      $("<td>").text(tDestination),
+      $("<td>").text(tFrequency),
+      $("<td>").text(tArrival),
+      $("<td>").text(tMinutes)
+    )
 
-    $("#train-table > tbody").append(newRow);
+    $("#train-table > tbody").append(newRow)
 
     // Handle the errors
-}, function (errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-});
-
+  },
+  function (errorObject) {
+    console.log("Errors handled: " + errorObject.code)
+  }
+)
